215746b3baab92a309c3b8394fafd662cbf5c5c8
Koji <sohailxzoraibi@gmail.com>
2025-07-04 22:53:33 +0100

    Add project files.

---
+++ LinkedListLibrary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "clsNode.h"
+
+using namespace std;
+
+
+int main(){
+	Node* FirstNode = new Node("Sohail", "Zoraibi", 20, nullptr);
+	Node* SecondNode = new Node("Rayane", "Zoraibi", 13, nullptr); 
+	Node* ThirdNode = new Node("Mouad", "Sodore", 18, nullptr);
+	Node* FourthNode = new Node("Walid", "Khayati", 15, nullptr);
+
+	FirstNode->Next = SecondNode; 
+	SecondNode->Next = ThirdNode;
+	ThirdNode->Next = FourthNode;
+	
+	Node* head = FirstNode;
+
+	Node* NewNode = new Node("Jocker", "Sohail", 2999, nullptr);
+	Node::InsertBefore(head, "Mouad", NewNode);
+	Node::InsertAtEnd(head, new Node("Safae", "o7esso", 19, nullptr));
+	Node::PrintNodes(head);
+}
---
+++ clsNode.h
@@ -0,0 +1,140 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+
+class Node
+{
+public:
+	string FirstName;
+	string LastName;
+	int Grade;
+	Node* Next;
+
+	Node();
+	Node(string FirstName, string LastName, int Grade, Node* Next)
+	{
+		this->FirstName = FirstName;
+		this->LastName = LastName;
+		this->Grade = Grade;
+		this->Next = Next;
+	}
+
+	static void InsertInBeginning(Node* &Head, Node* ToAddNode)
+	{
+		ToAddNode->Next = Head;
+		Head = ToAddNode;
+	}
+
+	static void InsertInBeginning(Node* &Head, string FirstName, string LastName, int Grade)
+	{
+		Node::InsertInBeginning(Head,  new Node(FirstName,LastName, Grade, nullptr));
+	}
+
+	static Node* Find(Node* head, string FirstName)
+	{
+		while (head != nullptr)
+		{
+			if (head->FirstName == FirstName)
+			{
+				return head;
+			}
+			head = head->Next;
+		}
+		return nullptr;
+	}
+
+	static void PrintNodes(Node* head)
+	{
+		while (head != nullptr)
+		{
+			cout << "FirstName: " << head->FirstName << endl;
+			cout << "LastName : " << head->LastName << endl;
+			cout << "Grade    : " << head->Grade << endl;
+			cout << "\n\n";
+			head = head->Next;
+		}
+	}
+
+	static void PrintNode(Node* CurrentNode)
+	{
+		cout << "FirstName: " << CurrentNode->FirstName << endl;
+		cout << "LastName : " << CurrentNode->LastName << endl;
+		cout << "Grade    : " << CurrentNode->Grade << endl;
+		cout << "\n\n";
+	}
+
+	static void InsertAfter(Node* head, string TargetName, Node* ToInsertNode)
+	{
+		Node* TargetNode = Node::Find(head, TargetName);
+		if (TargetNode == nullptr) return;
+
+		ToInsertNode->Next = TargetNode->Next;
+		TargetNode->Next = ToInsertNode;
+	}
+
+	static void InsertAfter(Node* head, string TargetName, string FirstName, string Lastname, int Grade)
+	{
+		InsertAfter(head, TargetName, new Node(FirstName, Lastname, Grade, nullptr));
+	}
+
+	static void InsertBefore(Node* &head, string TargetName, Node* ToInsertNode)
+	{
+		if (head == nullptr) return;
+
+		if (head->FirstName == TargetName)
+		{
+			ToInsertNode->Next = head;
+			head = ToInsertNode;
+			return;
+		}
+
+		Node* CurrentNode = head;
+		Node* PreviousNode = nullptr;
+		while (CurrentNode != nullptr)
+		{
+			if (CurrentNode->FirstName == TargetName)
+			{
+				break;
+			}
+			PreviousNode = CurrentNode;
+			CurrentNode = CurrentNode->Next;
+		}
+
+		if (CurrentNode == nullptr) return;
+
+
+		ToInsertNode->Next = CurrentNode;
+		if (PreviousNode != nullptr)
+			PreviousNode->Next = ToInsertNode;
+	}
+
+	static void InsertAtEnd(Node*& head, Node* ToInsertNode)
+	{
+		if (head == nullptr)
+		{
+			head = ToInsertNode;
+			return;
+		}
+
+		Node* Current = head;
+		
+		while (Current->Next != nullptr)
+		{
+			Current = Current->Next;
+		}
+
+		Current->Next = ToInsertNode;
+	}
+
+	static void InsertAtEnd(Node*& head, string FirstName, string LastName, int Grade)
+	{
+		InsertAtEnd(head, new Node(FirstName, LastName, Grade, nullptr));
+	}
+
+	static void InsertBefore(Node* head, string TargetName, string FirstName, string Lastname, int Grade)
+	{
+		InsertBefore(head, TargetName, new Node(FirstName, Lastname, Grade, nullptr));
+	}
+};
